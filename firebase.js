bb56a6f9e89c39947424af9a532bca3a94b52222
tsicroxE <arbanakus@gmail.com>
2018-09-27 15:40:19 -0700

    round three, this time without all the stupidity and awfulness

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { 
+	API_KEY, 
+	AUTH_DOMAIN, 
+	DATABASE_URL, 
+	PROJECT_ID, 
+	STORAGE_BUCKET, 
+	MESSAGING_SENDER_ID} from './.env';
+
+firebaseConfig = {
+    apiKey: `${API_KEY}`,
+    authDomain: `${AUTH_DOMAIN}`,
+    databaseURL: `${DATABASE_URL}`,
+    projectId: `${PROJECT_ID}`,
+    storageBucket: `${STORAGE_BUCKET}`,
+    messagingSenderId: `${MESSAGING_SENDER_ID}`
+  };
